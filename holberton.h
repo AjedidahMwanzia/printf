d962226ffe8b24a931814087c22a4c70c7785b9a
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2023-03-28 11:54:08 +0300

    get function

--- holberton.h
+++ holberton.h
@@ -1,25 +1,23 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
-
 #include <stdarg.h>
-/**
- * struct print - Structure containing identifiers
- * @t: type to print
- * @f: function to print
- */
-typedef struct print
-{
-	char *t;
-	int (*f)(va_list);
-} print_t;
 
-int _putchar(char c);
+/**
+* struct pairing - struct for pairing format to fuctions
+*
+* @a: the operator
+* @ptr: the fuction associated
+*/
+typedef struct pairing
+{
+	char a;
+	int (*ptr)();
+} pair;
+
+int print_p(void);
 int _printf(const char *format, ...);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_b(va_list b);
-int print_R(va_list R);
+int print_c(va_list arg);
+int print_s(va_list arg);
+int printy(const char *format, va_list arg);
 
 #endif
